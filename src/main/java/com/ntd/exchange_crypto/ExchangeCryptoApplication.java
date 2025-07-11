79cbc5e1792d34d16d2963c3b8ed3f2f659a45ed
Nguyen Tien Dat <tie.dat2004@gmail.com>
2025-07-11 16:12:58 +0700

    Initial commit

---
+++ src/main/java/com/ntd/exchange_crypto/ExchangeCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.ntd.exchange_crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeCryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntd/exchange_crypto/ExchangeCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntd.exchange_crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
